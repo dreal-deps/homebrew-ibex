8712b30f1d8d8d5589745f60bcb81db8f2253fd0
Soonho Kong <soonho.kong@tri.global>
2018-01-15 07:09:50 -0500

    Add upload_to_bintray.sh script

---
+++ upload_to_bintray.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+set -euo pipefail
+IFS=$'\n\t'
+
+ID=$1
+PASSWORD=$2
+VERSION=$3
+case `uname -r` in
+  14.*)
+    OSX_NAME="yosemite"
+    ;;
+  15.*)
+    OSX_NAME="el_capitan"
+    ;;
+  16.*)
+    OSX_NAME="sierra"
+    ;;
+  17.*)
+    OSX_NAME="high_sierra"
+    ;;
+  *)
+    OSX_NAME="unknown"
+    ;;
+esac
+
+BOTTLE_FILENAME=ibex-${VERSION}.${OSX_NAME}.bottle.tar.gz
+BINTRAY_URL=https://api.bintray.com/content/dreal/homebrew-ibex/ibex
+
+if [ -e ${BOTTLE_FILENAME} ]
+then
+  # Upload Files
+  curl -T ${BOTTLE_FILENAME} -u${ID}:${PASSWORD} ${BINTRAY_URL}/${VERSION}/${BOTTLE_FILENAME}
+  # Publish version
+  curl -X POST -u${ID}:${PASSWORD} ${BINTRAY_URL}/${VERSION}/publish
+  # Remove the bottle
+  rm -v ${BOTTLE_FILENAME}
+else 
+  echo "File not found: ${BOTTLE_FILENAME}"
+fi
