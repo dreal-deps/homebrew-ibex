1490a9cd286d9e55bd0fb8e32a2ef34deed1f057
Soonho Kong <soonho.kong@tri.global>
2018-10-11 17:44:44 -0400

    Add ibex@2.7.2

---
+++ ibex@2.7.2.rb
@@ -0,0 +1,74 @@
+class IbexAT272 < Formula
+  desc "C++ library for constraint processing over real numbers"
+  homepage "https://github.com/ibex-team/ibex-lib"
+  url "https://github.com/dreal-deps/ibex-lib/archive/ibex-2.7.2.tar.gz"
+  sha256 "9f2661a7457a8a03fa64470588698b5fbc9874097c7a20047fc4a6b02143241a"
+
+  bottle do
+    root_url "https://dl.bintray.com/dreal/homebrew-ibex"
+    cellar :any
+    # sha256 "6a42d264b0be91bfbcb3c02b78d6d89ec293ae64f18c5af83c20d232d12f3bd1" => :sierra
+    # sha256 "b2f13961709592c613210a8606f5eba26295c1fedb6853df4d0037c6685f04a3" => :high_sierra
+    # sha256 "e304c10242ae5219fe2a169af1ec1bbbf47b49d34f070bd9d05ac7ba632ce0e7" => :mojave
+  end
+
+  keg_only :versioned_formula
+
+  option :cxx11
+
+  depends_on "bison" => :build
+  depends_on "flex" => :build
+  depends_on "pkg-config" => :build
+  depends_on "dreal-deps/coinor/clp"
+
+  def install
+    ENV.cxx11
+    ENV.append "CXXFLAGS", "-std=c++11"
+    print prefix.to_s
+    args = %W[
+      --prefix=#{prefix}
+      --enable-shared
+      --with-optim
+      --with-solver
+      --with-affine-extended
+      --interval-lib=filib
+      --lp-lib=clp
+      --clp-path=#{HOMEBREW_PREFIX}
+    ]
+    system "./waf", "configure", *args
+    system "./waf", "install"
+
+    pkgshare.install %w[examples plugins/solver/benchs]
+    (pkgshare/"examples/symb01.txt").write <<~EOS
+      function f(x)
+        return ((2*x,-x);(-x,3*x));
+      end
+    EOS
+    inreplace "#{share}/pkgconfig/ibex.pc", prefix, opt_prefix
+  end
+
+  test do
+    cp_r (pkgshare/"examples").children, testpath
+
+    # so that pkg-config can remain a build-time only dependency
+    inreplace %w[makefile slam/makefile] do |s|
+      s.gsub! /CXXFLAGS.*pkg-config --cflags ibex./,
+              "CXXFLAGS := -std=c++11 -I#{include} -I#{include}/ibex "\
+                          "-I#{include}/ibex/3rd "\
+                          "`pkg-config --cflags clp`"
+      s.gsub! /LIBS.*pkg-config --libs  ibex./,
+              "LIBS := -L#{lib} -libex "\
+              "`pkg-config --libs clp`"
+    end
+
+    (1..8).each do |n|
+      system "make", "lab#{n}"
+      system "./lab#{n}"
+    end
+
+    (1..3).each do |n|
+      system "make", "-C", "slam", "slam#{n}"
+      system "./slam/slam#{n}"
+    end
+  end
+end
