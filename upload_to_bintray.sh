63616fe98f29c704509c23ce1a58dcbff7c7e387
Soonho Kong <soonho.kong@tri.global>
2019-06-12 20:49:52 -0400

    fix: upload_to_bintray.sh

--- upload_to_bintray.sh
+++ upload_to_bintray.sh
@@ -27,17 +27,19 @@ case `uname -r` in
     ;;
 esac
 
-BOTTLE_FILENAME=ibex@${FORMULA_VERSION}-${VERSION}.${OSX_NAME}.bottle.tar.gz
+BOTTLE_FILENAME=ibex@${FORMULA_VERSION}--${VERSION}.${OSX_NAME}.bottle.tar.gz
+BOTTLE_NEW_FILENAME=ibex@${FORMULA_VERSION}-${VERSION}.${OSX_NAME}.bottle.tar.gz
 BINTRAY_URL=https://api.bintray.com/content/dreal/homebrew-ibex/ibex
 
 if [ -e ${BOTTLE_FILENAME} ]
 then
+  mv ${BOTTLE_FILENAME} ${BOTTLE_NEW_FILENAME}
   # Upload Files
-  curl -T ${BOTTLE_FILENAME} -u${ID}:${PASSWORD} ${BINTRAY_URL}/${VERSION}/${BOTTLE_FILENAME}
+  curl -T ${BOTTLE_NEW_FILENAME} -u${ID}:${PASSWORD} ${BINTRAY_URL}/${VERSION}/${BOTTLE_NEW_FILENAME}
   # Publish version
   curl -X POST -u${ID}:${PASSWORD} ${BINTRAY_URL}/${VERSION}/publish
   # Remove the bottle
-  rm -v ${BOTTLE_FILENAME}
+  rm -v ${BOTTLE_NEW_FILENAME}
 else 
   echo "File not found: ${BOTTLE_FILENAME}"
 fi
