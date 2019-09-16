4057b084cb51108d691102cb3ae3265e25072d4e
Jamie Snape <jamie.snape@kitware.com>
2019-09-16 13:18:00 -0400

    clp@1.17: use coinutils formula

--- clp@1.17.rb
+++ clp@1.17.rb
@@ -26,10 +26,10 @@
 
 class ClpAT117 < Formula
   desc "Linear programming solver"
-  homepage "https://projects.coin-or.org/Clp"
-  url "https://github.com/coin-or/Clp/archive/releases/1.17.3.zip"
-  sha256 "1878ef4b4efa2408dff5c319ae26c402d742f6a2a71dccd9a50b5f62be130b78"
-  revision 1
+  homepage "https://github.com/coin-or/Clp"
+  url "https://github.com/coin-or/Clp/archive/releases/1.17.3.tar.gz"
+  sha256 "25f0692fe1daa492e7801770af6991506ae9a8c34a4cae358d017400a02dfcf8"
+  revision 2
 
   bottle do
     root_url "https://dl.bintray.com/dreal/homebrew-ibex"
@@ -38,115 +38,54 @@ class ClpAT117 < Formula
     sha256 "13d2c75590a45f8af7453f9329032ccd40c84f231d6e244de95d664133fb40c3" => :high_sierra
     sha256 "d53bc8f5505b57701e60dcabb1747bea7e5f393eca8a381ed1ff8a1318fd66e8" => :mojave
   end
-  
+
   keg_only :versioned_formula
 
-  depends_on "pkg-config" => :build
-  depends_on "gcc"
+  depends_on "pkg-config" => [:build, :test]
+  depends_on "coinutils"
   depends_on "openblas"
 
-  resource "coin-or-coin-utils" do
-    url "https://github.com/coin-or/CoinUtils/archive/releases/2.11.2.tar.gz"
-    sha256 "30c7f6c84dbb9f6e4fe5bbe4015ed15e2d1402146f8354cfc50c34d8735a49b1"
-  end
-
-  resource "coin-or-netlib-data" do
-    url "https://github.com/coin-or-tools/Data-Netlib/archive/releases/1.2.6.tar.gz"
-    sha256 "fac8b46bdc4b80eeb321ab437812d3bff1f2887c71822d6a26914ac8bfe3628e"
-  end
-
-  resource "coin-or-sample-data" do
-    url "https://github.com/coin-or-tools/Data-Sample/archive/releases/1.2.11.tar.gz"
-    sha256 "888d21a31e93a529eb3743a92f2ba62b94b3eed4ddc44351feb8034a84c71ec5"
+  resource "coin-or-tools-data-sample-p0033-mps" do
+    url "https://raw.githubusercontent.com/coin-or-tools/Data-Sample/releases/1.2.11/p0033.mps"
+    sha256 "8ccff819023237c79ef32e238a5da9348725ce9a4425d48888baf3a0b3b42628"
   end
 
   def install
-    resource("coin-or-netlib-data").stage do
-      args = %W[
-        --datadir=#{pkgshare}
-        --disable-debug
-        --disable-dependency-tracking
-        --disable-silent-rules
-        --prefix=#{prefix}
-      ]
-      system "./configure", *args
-      system "make"
-      system "make", "install"
-      rm_f lib/"pkgconfig/coindatanetlib.pc"
-    end
-
-    resource("coin-or-sample-data").stage do
-      args = %W[
-        --datadir=#{pkgshare}
-        --disable-debug
-        --disable-dependency-tracking
-        --disable-silent-rules
-        --prefix=#{prefix}
-      ]
-      system "./configure", *args
-      system "make"
-      system "make", "install"
-      rm_f lib/"pkgconfig/coindatasample.pc"
-    end
-
-    resource("coin-or-coin-utils").stage do
-      args = [
-        "--datadir=#{pkgshare}",
-        "--disable-debug",
-        "--disable-dependency-tracking",
-        "--disable-silent-rules",
-        "--prefix=#{prefix}",
-        "--includedir=#{include}/clp",
-        "--with-sample-datadir=#{pkgshare}/coin/Data/Sample",
-        "--with-netlib-datadir=#{pkgshare}/coin/Data/Netlib",
-        "--with-blas-incdir=#{Formula["openblas"].opt_include}",
-        "--with-blas-lib=-L#{Formula["openblas"].opt_lib} -lopenblas",
-        "--with-lapack-incdir=#{Formula["openblas"].opt_include}",
-        "--with-lapack-lib=-L#{Formula["openblas"].opt_lib} -lopenblas",
-      ]
-      system "./configure", *args
-      system "make"
-      ENV.deparallelize { system "make", "install" }
-      rm_f lib/"pkgconfig/coinutils.pc"
-    end
-
     args = [
       "--datadir=#{pkgshare}",
       "--disable-debug",
       "--disable-dependency-tracking",
+      "--disable-silent-rules",
       "--includedir=#{include}/clp",
       "--prefix=#{prefix}",
       "--with-blas-incdir=#{Formula["openblas"].opt_include}",
       "--with-blas-lib=-L#{Formula["openblas"].opt_lib} -lopenblas",
-      "--with-coinutils-incdir=#{include}/clp/coin",
-      "--with-coinutils-lib=-L#{lib} -lCoinUtils",
       "--with-lapack-incdir=#{Formula["openblas"].opt_include}",
       "--with-lapack-lib=-L#{Formula["openblas"].opt_lib} -lopenblas",
-      "--with-netlib-datadir=#{pkgshare}/coin/Data/Netlib",
-      "--with-sample-datadir=#{pkgshare}/coin/Data/Sample",
     ]
     system "./configure", *args
     system "make"
     ENV.deparallelize
     system "make", "install"
-    inreplace "#{lib}/pkgconfig/clp.pc", "-L#{lib} -lCoinUtils ", "-lCoinUtils"
     inreplace "#{lib}/pkgconfig/clp.pc", "includedir=#{prefix}", "includedir=${prefix}"
     inreplace "#{lib}/pkgconfig/clp.pc", prefix, opt_prefix
   end
 
   test do
-    system bin/"clp", "-import", pkgshare/"coin/Data/Sample/p0033.mps", "-primals"
+    resource("coin-or-tools-data-sample-p0033-mps").stage testpath
+    system bin/"clp", "-import", testpath/"p0033.mps", "-primals"
     (testpath/"test.cpp").write <<~EOS
       #include <ClpSimplex.hpp>
       int main() {
         ClpSimplex model;
-        int status = model.readMps("#{pkgshare}/coin/Data/Sample/p0033.mps", true);
+        int status = model.readMps("#{testpath}/p0033.mps", true);
         if (status != 0) { return status; }
         status = model.primal();
         return status;
       }
     EOS
-    system ENV.cxx, "test.cpp", "-I#{opt_include}/clp/coin", "-L#{opt_lib}", "-lClp", "-lClpSolver", "-lCoinUtils"
+    pkg_config_flags = `pkg-config --cflags --libs clp`.chomp.split
+    system ENV.cxx, "test.cpp", *pkg_config_flags
     system "./a.out"
   end
 end
