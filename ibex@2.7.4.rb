eec60642b06337058f03ecb757305176a8bfebd8
Soonho Kong <soonho.kong@tri.global>
2019-09-17 11:22:53 -0400

    ibex uses homebrew-core/clp

--- ibex@2.7.4.rb
+++ ibex@2.7.4.rb
@@ -2,15 +2,15 @@ class IbexAT274 < Formula
   desc "C++ library for constraint processing over real numbers"
   homepage "https://github.com/ibex-team/ibex-lib"
   url "https://github.com/dreal-deps/ibex-lib/archive/ibex-2.7.4.tar.gz"
-  revision 4
+  revision 6
   sha256 "60c8248fe4669f8634ba3ea5190d06f740215b8f0170bf67bfb41705fa3a5a4c"
 
   bottle do
     root_url "https://dl.bintray.com/dreal/homebrew-ibex"
     cellar :any
-    sha256 "c54ce0afe76b0c60c3241428c77844b58c494a2514816cb285a77eaec8004de9" => :sierra
-    sha256 "df58b64c97622dc4f7eb44dd679664488170ae888ebb82039be5692d3770c1b4" => :high_sierra
-    sha256 "73ffe8ce3a5abe3f2098ccb4b977ca59b13e8601cb3fd284be229238b612c83f" => :mojave
+    sha256 "62de67207718790ffcf7ac2cdbf05d7507535400ffbce51fc89cd2fcba37324a" => :sierra
+    sha256 "c93cfb274cc7f0c0b42ee22cc74e62e54c72116742f1e3fc3c0ae4a236d1e94b" => :high_sierra
+    sha256 "20e1df9c9c9931c09889cdd9e1965786309ea7b83e11af5d1fd22458ae7dcf80" => :mojave
   end
 
   keg_only :versioned_formula
@@ -18,7 +18,7 @@ class IbexAT274 < Formula
   depends_on "bison" => :build
   depends_on "flex" => :build
   depends_on "pkg-config" => :build
-  depends_on "clp@1.17"
+  depends_on "clp"
 
   def install
     ENV.cxx11
@@ -32,7 +32,7 @@ class IbexAT274 < Formula
       --with-affine-extended
       --interval-lib=filib
       --lp-lib=clp
-      --clp-path=#{Formula["clp@1.17"].opt_prefix}
+      --clp-path=#{Formula["clp"].opt_prefix}
     ]
     system "./waf", "configure", *args
     system "./waf", "install"
@@ -54,10 +54,10 @@ class IbexAT274 < Formula
       s.gsub! /CXXFLAGS.*pkg-config --cflags ibex./,
               "CXXFLAGS := -std=c++11 -I#{include} -I#{include}/ibex "\
                           "-I#{include}/ibex/3rd "\
-                          "`PKG_CONFIG_PATH=#{Formula["clp@1.17"].opt_lib}/pkgconfig pkg-config --cflags clp`"
+                          "`PKG_CONFIG_PATH=#{Formula["clp"].opt_lib}/pkgconfig pkg-config --cflags clp`"
       s.gsub! /LIBS.*pkg-config --libs  ibex./,
               "LIBS := -L#{lib} -libex "\
-              "`PKG_CONFIG_PATH=#{Formula["clp@1.17"].opt_lib}/pkgconfig pkg-config --libs clp`"
+              "`PKG_CONFIG_PATH=#{Formula["clp"].opt_lib}/pkgconfig pkg-config --libs clp`"
     end
 
     (1..8).each do |n|
