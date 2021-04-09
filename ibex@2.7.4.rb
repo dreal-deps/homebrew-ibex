98ecc6d9a21ce77b1edba71e2ec3eb784cadf0bc
Soonho Kong <soonho.kong@tri.global>
2021-04-09 11:30:14 -0400

    Fix style

--- ibex@2.7.4.rb
+++ ibex@2.7.4.rb
@@ -1,3 +1,6 @@
+# typed: false
+# frozen_string_literal: true
+
 class IbexAT274 < Formula
   desc "C++ library for constraint processing over real numbers"
   homepage "https://github.com/ibex-team/ibex-lib"
@@ -8,11 +11,10 @@ class IbexAT274 < Formula
 
   bottle do
     root_url "https://dl.bintray.com/dreal/homebrew-ibex"
-    cellar :any
-    sha256 "3feb8467dffec3e9a677e1ba97e8d8af4772f601a009e410cc4014d4dd2593e1" => :high_sierra
-    sha256 "5e99662f25ae823c1998e0112d9265ff84b43671e8f7cdb769ae7a101428981b" => :mojave
-    sha256 "7ad28ea8e6e974e14d02971b0742738f27bad4c2a4cd173ef09276ae543dfc8e" => :catalina
-    sha256 "aeb335dd6f214b9fecbfac38c0d0befcf20cc5022047045bed6c449ff8a39ff3" => :big_sur
+    sha256 cellar: :any, high_sierra: "3feb8467dffec3e9a677e1ba97e8d8af4772f601a009e410cc4014d4dd2593e1"
+    sha256 cellar: :any, mojave:      "5e99662f25ae823c1998e0112d9265ff84b43671e8f7cdb769ae7a101428981b"
+    sha256 cellar: :any, catalina:    "7ad28ea8e6e974e14d02971b0742738f27bad4c2a4cd173ef09276ae543dfc8e"
+    sha256 cellar: :any, big_sur:     "aeb335dd6f214b9fecbfac38c0d0befcf20cc5022047045bed6c449ff8a39ff3"
   end
 
   keg_only :versioned_formula
@@ -53,13 +55,13 @@ class IbexAT274 < Formula
 
     # so that pkg-config can remain a build-time only dependency
     inreplace %w[makefile slam/makefile] do |s|
-      s.gsub! /CXXFLAGS.*pkg-config --cflags ibex./,
+      s.gsub!(/CXXFLAGS.*pkg-config --cflags ibex./,
               "CXXFLAGS := -I#{include} -I#{include}/ibex "\
                           "-I#{include}/ibex/3rd "\
-                          "`PKG_CONFIG_PATH=#{Formula["clp"].opt_lib}/pkgconfig pkg-config --cflags clp`"
-      s.gsub! /LIBS.*pkg-config --libs  ibex./,
+                          "`PKG_CONFIG_PATH=#{Formula["clp"].opt_lib}/pkgconfig pkg-config --cflags clp`")
+      s.gsub!(/LIBS.*pkg-config --libs  ibex./,
               "LIBS := -L#{lib} -libex "\
-              "`PKG_CONFIG_PATH=#{Formula["clp"].opt_lib}/pkgconfig pkg-config --libs clp`"
+              "`PKG_CONFIG_PATH=#{Formula["clp"].opt_lib}/pkgconfig pkg-config --libs clp`")
     end
 
     (1..8).each do |n|
